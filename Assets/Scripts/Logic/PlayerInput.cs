c8ae7d15813b31af74de3694307f2015dd1e2179
ZoryaGs <dontneedyou123@mail.ru>
2023-08-21 22:20:52 +0300

    Update 0.12

---
+++ Assets/Scripts/Logic/PlayerInput.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : InputController
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    private void Update()
+    {
+        Move(new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical")));
+    }
+}
