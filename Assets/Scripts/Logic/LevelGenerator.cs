0a9799ed7560030fd21cdb0ace8fa25710c0991d
AlexKvitko <sashaishappy1@mail.ru>
2023-09-30 13:19:44 +0300

    Update 0.18.9

--- Assets/Scripts/Logic/LevelGenerator.cs
+++ Assets/Scripts/Logic/LevelGenerator.cs
@@ -16,6 +16,18 @@ public class LevelGenerator : MonoBehaviour
 
     private List<Vector3> vertices;
 
+    public int MaxResources;
+
+    public float Radius;
+
+    public int BaseRadius;
+
+    public int MaxBorder;
+
+    public GameObject Wood;
+
+    public GameObject Rock;
+
     private void Start()
     {
         meshCollider = MapPlane.GetComponent<MeshCollider>();
@@ -41,6 +53,25 @@ public class LevelGenerator : MonoBehaviour
         mesh.SetVertices(vertices);
 
         meshCollider.sharedMesh = mesh;
+
+        for(int i = 0; i < mapOffset.random.Next(MaxResources - 20, MaxResources); i++)
+        {
+        begin:
+            Vector3 position = new Vector3((BaseRadius + mapOffset.random.Next(MaxBorder)) * mapOffset.GenerateSign(), 0.5f, (BaseRadius + mapOffset.random.Next(MaxBorderw)) * mapOffset.GenerateSign());
+
+            if(Physics.CheckSphere(position, Radius, 1 << 9))
+                goto begin;
+
+            switch (mapOffset.random.Next(3))
+            {
+                case 1:
+                    Instantiate(Wood,position,Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(Rock, position, Quaternion.identity);
+                    break;
+            }
+        }
     }
 
     /*
@@ -55,17 +86,27 @@ public class LevelGenerator : MonoBehaviour
         private byte[] offsets;
         private int current;
 
+        public Random random;
+
         public MapOffset(int count, int seed)
         {
             offsets = new byte[count];
             current = 0;
 
-            Random random = new Random(seed);
+            random = new Random(seed);
             random.NextBytes(offsets);
         }
 
         public byte Next() => offsets[current++];
 
         public float NextF() => (offsets[current++] - 127) / 1280f;
+
+        public int GenerateSign()
+        {
+            if (random.Next(0, 2) == 0)
+                return -1;
+            else
+                return 1;
+    }
     }
 }
